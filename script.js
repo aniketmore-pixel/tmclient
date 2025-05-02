95aa197cc0ae53f35883ade3ec5f1926180deb03
Aniket More <aniketmore.personal@gmail.com>
2025-05-02 22:49:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function searchRecipes(reset = true) {
     return;
   }
 
-  fetch("http://127.0.0.1:5000/search", {
+  fetch("https://tastemate-backend-jvnc.onrender.com/search", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
